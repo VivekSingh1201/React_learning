d3894d0f5df01e802d91d34be35aa8f13842c57a
Vivek Singh <vs6000675@gmail.com>
2025-04-03 16:36:42 +0530

    modified:   01Project/src/main.jsx

--- 01Project/src/main.jsx
+++ 01Project/src/main.jsx
@@ -1,21 +1,21 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 // import App from './App.jsx'
 // import Chai from './chai.jsx'
 
-// function MyApp(){
-//   return (
-//     <div>
-//       <h1>React App</h1>
-//     </div>
-//   )
-// }
+/*
+function MyApp(){
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  )
+}
+  */
 
 const reactElement = React.createElement(
   'a',
-  {href: 'https://reactjs.org', target: '_blank'},
-  'Learn React'
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to go to Google'
 )
 
 createRoot(document.getElementById('root')).render(
